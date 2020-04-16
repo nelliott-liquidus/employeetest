9c2b1ecd336357af3ce4b705e263263508131a49
Nick Elliott <n.elliott@fusion92.com>
2020-04-16 10:19:08 -0500

    initial commit

---
+++ src/employeeTypes.js
@@ -0,0 +1,96 @@
+const _ = require('lodash')
+const inquirer = require('inquirer')
+
+let id = 0 // auto increment
+
+class Employee {
+
+  constructor(options) {
+    this.defaults = {
+      id: id,
+      name: '',
+      role: 'Employee',
+      propsToEnter: ['name']
+    }
+    this._options = options || {}
+    id++
+  }
+
+  get(key) {
+    return _.get(this._options, key, _.get(this.defaults, key))
+  }
+
+  get options() {
+    return _.mapValues(this.defaults, (val, key) => this.get(key))
+  }
+
+  setOption(key, val) {
+    if(!Object.keys(this.defaults).includes(key)) {
+      this.defaults[key] = null // add key to defaults if a new key is being added to _options
+    }
+    this._options[key] = val
+  }
+
+  setOptions(obj) { // set multiple options via object
+    Object.keys(obj).forEach(key => this.setOption(key, obj[key]))
+  }
+
+  async ask() {
+    this.setOptions(
+      await inquirer.prompt(
+        this.options.propsToEnter.map(prop => {
+          return {
+            type: 'input',
+            name: prop,
+            message: `Enter ${ this.constructor.name } ${ prop }: `
+          }
+        })
+      )
+    )
+  }
+
+  render() { // render a div for each option,
+    return `
+      <div class="employee">
+        ${ Object.keys(_.omit(this.options, ['propsToEnter'])).map(option => `<div class="${ option }">${ this.options[option] }</div>`).join('') }
+      </div>
+    `
+  }
+
+}
+
+class Manager extends Employee {
+  constructor(options) {
+    super(options)
+    this.setOption('role', 'Manager')
+    this.setOption('propsToEnter', [...super.options.propsToEnter, 'officeNumber'])
+  }
+}
+
+class Engineer extends Employee {
+  constructor(options) {
+    super(options)
+    this.setOption('role', 'Engineer')
+    this.setOption('propsToEnter', [...super.options.propsToEnter, 'github'])
+  }
+
+  render() {
+    this.setOption('githubData', 'github data') // actually get github info here
+    return super.render()
+  }
+}
+
+class Intern extends Employee {
+  constructor(options) {
+    //options.special = [{ prop: 'Office Number', val: 0 }]
+    super(options)
+    this.setOption('role', 'Intern')
+    this.setOption('propsToEnter', [...super.options.propsToEnter, 'school'])
+  }
+}
+
+module.exports = {
+  Manager,
+  Engineer,
+  Intern,
+}
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer')
+const _ = require('lodash')
+
+let EmployeeTypes = require('./employeeTypes.js')
+let team = []
+
+async function main() {
+
+  async function createEmployee(type) {
+    try {
+      const role = type || await inquirer.prompt({
+        type: 'input',
+        name: 'type',
+        message: `Enter role (${ Object.keys(_.omit(EmployeeTypes, ['Manager'])) })`,
+      })
+      if(!Object.keys(EmployeeTypes).includes(role.type)) {
+        console.log('Invalid employee type.')
+      }
+      else {
+        team.push(new EmployeeTypes[role.type]())
+        await team[team.length - 1].ask()
+      }
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+  async function loop() { // adds employees til the user doesn't want to add any more
+    const enterNew = await inquirer.prompt({
+      type: 'input',
+      name: 'res',
+      message: 'Enter new employee? (y/n)',
+    })
+    if(enterNew.res.toLowerCase() == 'y') {
+
+      await createEmployee()
+      loop()
+    }
+    else {
+      renderTeam()
+    }
+  }
+
+  // We have to have a manager so we add that first
+  await createEmployee({ 'type': 'Manager' })
+  loop()
+}
+
+
+
+main()
